b2cafef2a55f9728429deb6fed5a1b2d3abababa
GABIBACK13 <gabrielback2006oficial@gmail.com>
2025-03-15 23:29:56 -0300

    Typescript study starts
    config of typescript, prettier, and the development configs

---
+++ CursoTS/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const helloFunc_1 = __importDefault(require("./helloFunc"));
+(0, helloFunc_1.default)();
